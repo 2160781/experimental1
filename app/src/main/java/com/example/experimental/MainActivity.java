39745b9fa9b5caacced52384f76482729bfeb28e
Francisco Canto <2160781@my.ipleiria.pt>
2021-05-13 11:44:04 +0100

    Teste com base no método do Professor Carlos

--- app/src/main/java/com/example/experimental/MainActivity.java
+++ app/src/main/java/com/example/experimental/MainActivity.java
@@ -1,62 +1,104 @@
 package com.example.experimental;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.ActivityNotFoundException;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.speech.RecognizerIntent;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import java.text.BreakIterator;
 import java.util.ArrayList;
-import java.util.Locale;
-
-public class MainActivity extends AppCompatActivity {
+import android.util.Log;
 
 
-    TextView outputText;
+
+public class MainActivity extends Activity implements OnClickListener
+{
+
+    private TextView mText;
+    private SpeechRecognizer sr;
+    private static final String TAG = "MyActivity";
+    private BreakIterator recognisedText;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        outputText = (TextView) findViewById(R.id.txt_output);
-
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hi Speak Something");
-
-        try {
-            startActivityForResult(intent, 1);
-        }catch (ActivityNotFoundException e){
-            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
-        }
+        Button speakButton = (Button) findViewById(R.id.button);
+        mText = (TextView) findViewById(R.id.txt_output);
+        speakButton.setOnClickListener(this);
+        sr = SpeechRecognizer.createSpeechRecognizer(this);
+        sr.setRecognitionListener(new listener());
     }
 
+    class listener implements RecognitionListener
+    {
+        public void onReadyForSpeech(Bundle params)
+        {
+            Log.d(TAG, "onReadyForSpeech");
+        }
+        public void onBeginningOfSpeech()
+        {
+            Log.d(TAG, "onBeginningOfSpeech");
+        }
+        public void onRmsChanged(float rmsdB)
+        {
+            Log.d(TAG, "onRmsChanged");
+        }
+        public void onBufferReceived(byte[] buffer)
+        {
+            Log.d(TAG, "onBufferReceived");
+        }
+        public void onEndOfSpeech()
+        {
+            Log.d(TAG, "onEndofSpeech");
+        }
+        public void onError(int error)
+        {
+            Log.d(TAG,  "error " +  error);
+            mText.setText("error " + error);
+        }
+        public void onResults(Bundle results)
+        {
+            String str = new String();
+            Log.d(TAG, "onResults " + results);
+            ArrayList data = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            for (int i = 0; i < data.size(); i++)
+            {
+                Log.d(TAG, "result " + data.get(i));
+                str += data.get(i);
+            }
+            mText.setText("results: "+String.valueOf(data.size()));
+        }
+        public void onPartialResults(Bundle partialResults)
+        {
+            ArrayList data = partialResults.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            String word = (String) data.get(data.size() - 1);
+            recognisedText.setText(word);
 
+            Log.i("TEST", "partial_results: " + word);
+        }
+        public void onEvent(int eventType, Bundle params)
+        {
+            Log.d(TAG, "onEvent " + eventType);
+        }
+    }
+    public void onClick(View v) {
+        if (v.getId() == R.id.button)
+        {
+            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+            intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,"voice.recognition.test");
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+            intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS,5);
+            sr.startListening(intent);
 
-        switch (requestCode){
-
-            case 1:
-
-                if(resultCode == RESULT_OK && null != data){
-
-                    ArrayList<String> result =
-                            data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-
-                    outputText.setText(result.get(0));
-                }
-
-                break;
         }
     }
 }
